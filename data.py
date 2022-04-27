73c635e86a9597da1f69d573c46946fdffd505d0
Furry Chemistry <84753790+aryazakaria01@users.noreply.github.com>
2022-04-28 06:46:09 +0700

    Fixed

---
+++ data.py
@@ -0,0 +1,61 @@
+from pyrogram.types import InlineKeyboardButton
+
+
+class Data:
+    generate_single_button = [InlineKeyboardButton("🔥 Start Generating Session 🔥", callback_data="generate")]
+
+    home_buttons = [
+        generate_single_button,
+        [InlineKeyboardButton(text="🏠 Return Home 🏠", callback_data="home")]
+    ]
+
+    generate_button = [generate_single_button]
+
+    buttons = [
+        generate_single_button,
+        [InlineKeyboardButton("Developer", url="https://t.me/xflskyy")],
+        [
+            InlineKeyboardButton("How to Use ❔", callback_data="help"),
+            InlineKeyboardButton("🎪 About 🎪", callback_data="about")
+        ],
+        [InlineKeyboardButton("Group Support", url="https://t.me/NastySupportt")],
+    ]
+
+    START = """
+Hey {}
+
+Welcome to {}
+
+If you don't trust this bot, 
+1) stop reading this message
+2) delete this chat
+
+Still reading?
+You can use me to generate pyrogram (even version 2) and telethon string session. Use below buttons to learn more !
+
+By @xflskyy
+    """
+
+    HELP = """
+✨ **Available Commands** ✨
+/about - About The Bot
+/help - This Message
+/start - Start the Bot
+/generate - Generate Session
+/cancel - Cancel the process
+/restart - Cancel the process
+"""
+
+    ABOUT = """
+**About This Bot** 
+
+Telegram Bot to generate Pyrogram and Telethon string session by @xflskyy
+
+Group Support : [Click Here](https://t.me/NastySupportt)
+
+Framework : [Pyrogram](https://docs.pyrogram.org)
+
+Language : [Python](https://www.python.org)
+
+Developer : @xflskyy
+    """
